141e0f4532b8e0e4558884b5551ae1995db4b8f8
Samuel Pelletier <samuel@kaviju.com>
2014-06-18 09:48:30 -0400

    Adding a cascade delete rule.

--- Sources/com/kaviju/accesscontrol/migration/KAAccessControlMigrationHelper.java
+++ Sources/com/kaviju/accesscontrol/migration/KAAccessControlMigrationHelper.java
@@ -4,10 +4,6 @@ import er.extensions.migration.*;
 public class KAAccessControlMigrationHelper {
 
 	static public void addForeignKeyAndIndex(ERXMigrationDatabase database, String realUserTableName) throws Throwable {
-		
-		ERXMigrationTable kaUserTable = database.existingTableNamed(realUserTableName);
-		kaUserTable.addUniqueIndex("userName");
-
 		ERXMigrationTable kaUserProfileTable = database.existingTableNamed("KAUserProfile");
 		kaUserProfileTable.addForeignKey("user_id", realUserTableName, "id");
 
