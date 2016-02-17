12497f0e8eeee48ca4203ee4a193111af784ff34
Samuel Pelletier <samuel@kaviju.com>
2016-02-17 10:35:59 -0500

    Add a way to get all the users with a specific role.

--- Sources/com/kaviju/accesscontrol/model/KAAccessListItem.java
+++ Sources/com/kaviju/accesscontrol/model/KAAccessListItem.java
@@ -2,8 +2,19 @@ package com.kaviju.accesscontrol.model;
 
 import org.apache.log4j.Logger;
 
+import com.webobjects.foundation.NSArray;
+
 @SuppressWarnings("serial")
 public class KAAccessListItem extends com.kaviju.accesscontrol.model.base._KAAccessListItem {
 	@SuppressWarnings("unused")
 	private static Logger log = Logger.getLogger(KAAccessListItem.class);
+	
+	public NSArray<? extends KAUserProfile>fetchUserProfileWithItemForRole(String roleCode) {
+		KARole role = KARole.roleWithCode(editingContext(), roleCode);
+		
+		NSArray<KAUserProfileRole> userProfileRoles = KAUserProfileRole.fetchKAUserProfileRoles(editingContext(), KAUserProfileRole.ROLE.eq(role), null);
+		userProfileRoles = KAUserProfileRole.LIST_ITEMS.containsObject(this).filtered(userProfileRoles);
+		
+		return KAUserProfileRole.USER_PROFILE.arrayValueInObject(userProfileRoles);
+	}
 }
