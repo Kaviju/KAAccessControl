8472cdf128c3b08252d73cae39a3ee12a1e13ed8
Samuel Pelletier <samuel@kaviju.com>
2018-05-08 10:38:26 -0400

    Add a digits only password generator.

--- Sources/com/kaviju/accesscontrol/authentication/RandomPasswordGenerator.java
+++ Sources/com/kaviju/accesscontrol/authentication/RandomPasswordGenerator.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class RandomPasswordGenerator {
 	static final String possibleChars = "23456789abcdefghjklmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ@#$%?&*()-+";
+	static final String possibleDigits = "0123456789";
 	static Random rnd = new Random();
 
 	static public String newPassword(int len) {
@@ -12,6 +13,17 @@ public class RandomPasswordGenerator {
 			for( int i = 0; i < len; i++ ) {
 				int nextCharIndex = rnd.nextInt(possibleChars.length());
 				password.append(possibleChars.charAt(nextCharIndex));
+			}
+			return password.toString();
+		}		
+	}
+
+	static public String newDigitsPassword(int len) {
+		{
+			StringBuilder password = new StringBuilder( len );
+			for( int i = 0; i < len; i++ ) {
+				int nextCharIndex = rnd.nextInt(possibleDigits.length());
+				password.append(possibleDigits.charAt(nextCharIndex));
 			}
 			return password.toString();
 		}		
