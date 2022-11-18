cac6c8b3c03323210721e6453d9b09c672bad57e
Samuel Pelletier <samuel@kaviju.com>
2022-11-18 11:09:42 -0500

    Add a random generator with only alphanumeric chars.

--- Sources/com/kaviju/accesscontrol/authentication/RandomPasswordGenerator.java
+++ Sources/com/kaviju/accesscontrol/authentication/RandomPasswordGenerator.java
@@ -3,9 +3,21 @@ package com.kaviju.accesscontrol.authentication;
 import java.util.Random;
 
 public class RandomPasswordGenerator {
+	static final String possibleAlphaChars = "23456789abcdefghjklmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ";
 	static final String possibleChars = "23456789abcdefghjklmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ@#$%?&*()-+";
 	static final String possibleDigits = "0123456789";
 	static Random rnd = new Random();
+
+	static public String newAlphanumericPassword(int len) {
+		{
+			StringBuilder password = new StringBuilder( len );
+			for( int i = 0; i < len; i++ ) {
+				int nextCharIndex = rnd.nextInt(possibleAlphaChars.length());
+				password.append(possibleAlphaChars.charAt(nextCharIndex));
+			}
+			return password.toString();
+		}		
+	}
 
 	static public String newPassword(int len) {
 		{
